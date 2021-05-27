404461621a5b7dda1c930e7dfb62e5e49dd0838f
MANABE <k.manabe0218@gmail.com>
2021-05-28 00:07:48 +0900

    答え合わせ済み

--- 100knock.py
+++ 100knock.py
@@ -28,27 +28,27 @@ df_geocode = pd.read_csv(
 
 # %%
 # P-001: レシート明細のデータフレーム（df_receipt）から全項目の先頭10件を表示し、どのようなデータを保有しているか目視で確認せよ。
-print(df_receipt.head(10))
+df_receipt.head(10)
 
 # %%
 # P-002: レシート明細のデータフレーム（df_receipt）から売上日（sales_ymd）、顧客ID（customer_id）、商品コード（product_cd）、売上金額（amount）の順に列を指定し、10件表示させよ。
 
 listP002 = ["sales_ymd", "customer_id", "product_cd", "amount"]
-
-for i in listP002:
-    print(df_receipt[i].head(10))
+df_receipt[listP002].head(10)
 
 # %%
 # P-003: レシート明細のデータフレーム（df_receipt）から売上日（sales_ymd）、顧客ID（customer_id）、商品コード（product_cd）、売上金額（amount）の順に列を指定し、10件表示させよ。ただし、sales_ymdはsales_dateに項目名を変更しながら抽出すること。
 df_receipt = df_receipt.rename(columns={"sales_ymd": "sales_date"})
 listP002 = ["sales_date", "customer_id", "product_cd", "amount"]
 
-print(df_receipt[listP002].head(10))
+df_receipt[listP002].head(10)
 
 # %%
 # P-004: レシート明細のデータフレーム（df_receipt）から売上日（sales_ymd）、顧客ID（customer_id）、商品コード（product_cd）、売上金額（amount）の順に列を指定し、以下の条件を満たすデータを抽出せよ。
 df_receipt = df_receipt.rename(columns={"sales_date": "sales_ymd"})
 listP002 = ["sales_ymd", "customer_id", "product_cd", "amount"]
 
-print(df_receipt[df_receipt.customer_id == "CS018205000001"][listP002])
+#df_receipt[df_receipt.customer_id == "CS018205000001"][listP002]
+
+df_receipt[listP002].query('customer_id == "CS018205000001"')
 # %%
