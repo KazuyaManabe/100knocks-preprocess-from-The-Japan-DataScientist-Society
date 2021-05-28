854b59f998f9129b03aeb209ba86898b2d721a60
MANABE <k.manabe0218@gmail.com>
2021-05-28 23:30:32 +0900

    5本目から

--- 100knock.py
+++ 100knock.py
@@ -52,3 +52,17 @@ listP002 = ["sales_ymd", "customer_id", "product_cd", "amount"]
 
 df_receipt[listP002].query('customer_id == "CS018205000001"')
 # %%
+#P-005: レシート明細のデータフレーム（df_receipt）から売上日（sales_ymd）、顧客ID（customer_id）、商品コード（product_cd）、売上金額（amount）の順に列を指定し、以下の条件を満たすデータを抽出せよ。
+df_receipt[listP002].query('customer_id == "CS018205000001"').query('amount >= 1000')
+
+# %%
+#P-006: レシート明細データフレーム「df_receipt」から売上日（sales_ymd）、顧客ID（customer_id）、商品コード（product_cd）、売上数量（quantity）、売上金額（amount）の順に列を指定し、以下の条件を満たすデータを抽出せよ。
+#顧客ID（customer_id）が"CS018205000001"
+#売上金額（amount）が1, 000以上または売上数量（quantity）が5以上
+
+listP006 = ["sales_ymd", "customer_id", "product_cd","quantity","amount"]
+
+df_receipt[listP006].query(
+    'customer_id == "CS018205000001"').query('amount >= 1000 or quantity >= 5')
+
+# %%
